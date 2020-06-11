a781567d8089e3d82a40b97d40d00785a2c3e568
Heejeong Kim <sophisticated97@naver.com>
2020-06-11 17:08:21 +0900

    로그인 기능 구현

--- src/com/yourcloset/user/action/LoginAction.java
+++ src/com/yourcloset/user/action/LoginAction.java
@@ -2,7 +2,6 @@ package com.yourcloset.user.action;
 
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,30 +16,29 @@ public class LoginAction implements CommandAction {
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		PrintWriter out = response.getWriter();
 
-		String id = request.getParameter("user_id");
-		String pass = request.getParameter("password");
-		String password = MD5.getMD5(pass);
+		String id = request.getParameter("id");
+		String password = MD5.getMD5(request.getParameter("password"));
 		
 		UserDAO userDAO = new UserDAOImpl();
 		UserVO user = null;
 
-		out.println("<script>");
+		out.println("<html><head><script>");
 		if (userDAO.login(id, password) != null) {
 			user = userDAO.selectUserByUserId(id);
 			HttpSession session = request.getSession();
 			session.setAttribute("user", user);
 			
 			out.println("alert('로그인 성공! 사용자 페이지로 이동합니다.')");
-			out.println("<script>");
+			out.println("</script></head></html>");
 
 			if (user.getPosition().equals("customer"))
-				return "profile.jsp"; 
-			else return "profileManager.jsp";
+				return "/mypage/profile.jsp"; 
+			else return "/manage/profileManager.jsp";
 
 		} else{
 			out.println("alert(' 로그인 실패! 로그인 페이지로 이동합니다. ')");
 			out.println("location.href='login.jsp'");	
-			out.println("<script>");
+			out.println("</script></head></html>");
 			return "login.jsp";
 		}
 	}
