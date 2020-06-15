bb7867b4eff8eec4657882590db9a9e88c961826
Heejeong Kim <sophisticated97@naver.com>
2020-06-15 12:08:29 +0900

    Properties를 이용한 JDBC 접근

--- src/com/yourcloset/utils/JdbcAgent.java
+++ src/com/yourcloset/utils/JdbcAgent.java
@@ -1,39 +1,51 @@
 package com.yourcloset.utils;
 
 import java.sql.*;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class JdbcAgent {
-	private String server = "127.0.0.1";
-	private String port = "3306";
-	private String database = "yourcloset";
-	private String username = "root";
-	private String password = "Mysql123!@#";
-	private String query = "serverTimezone=Asia/Seoul&characterEncoding=utf-8";
-	private String url = String.format("jdbc:mysql://%s:%s/%s?%s", server, port, database, query);
-	
 	private Connection con = null;
 
 	public JdbcAgent() {
+		Properties prop = loadProperties("com/yourcloset/properties/database");
+		String driverClassName = prop.getProperty("jdbc.driverClassName");
+		String url = prop.getProperty("jdbc.url");
+		String username = prop.getProperty("jdbc.username");
+		String password = prop.getProperty("jdbc.password");
+
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName(driverClassName);
 			con = DriverManager.getConnection(url, username, password);
-		}
-		catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			System.err.println("* Driver Load 오류; " + e.getMessage());
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			System.err.println("* DB 연결 실패; " + e.getMessage());
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.err.println("* 예상치 못한 오류 발생; " + e.getMessage());
 		}
 	}
 
+	private Properties loadProperties(String path) {
+		ResourceBundle rbHome = ResourceBundle.getBundle(path);// 누구를 실행할지를 rb에
+		Enumeration<String> keys = rbHome.getKeys();
+		Properties prop = new Properties();
+
+		while (keys.hasMoreElements()) {
+			String property = keys.nextElement();
+			String value = rbHome.getString(property);
+			prop.setProperty(property, value);
+		}
+		
+		return prop;
+	}
+
 	public void close() {
 		try {
-			if (con != null) con.close();
-		} 
-		catch (SQLException e) {
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
@@ -41,24 +53,22 @@ public class JdbcAgent {
 	public Connection getCon() {
 		return con;
 	}
-	
+
 	public static void main(String[] args) throws SQLException {
 		/* Connection Test */
 		Statement stmt = null;
 		ResultSet rs = null;
-		
-		String sql = "select id, name from user";
+		String sql = "select user_id, user_name from user";
 		JdbcAgent agent = new JdbcAgent();
-		
+
 		stmt = agent.getCon().createStatement();
 		rs = stmt.executeQuery(sql);
-		
-		while(rs.next()) {
+
+		while (rs.next()) {
 			String id = rs.getString("id");
 			String name = rs.getString("name");
 			System.out.println(String.format("User(ID:%s, NAME:%s)", id, name));
 		}
-		
+
 	}
 }
-
